4dbb2f5e25859a33261cb947e98c43c0b763e5b5
Dosoa <tug63171@temple.edu>
2019-10-22 18:10:34 -0400

    Lab	8 Submission
    Lab	8 Submission

---
+++ Dull_Lab_8/Dull_Lab_8/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dull_Lab_8
+{
+   public static  class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            
+
+
+      //      1. CreateBoard()
+      //2. CurrentPlayer()
+      //3. ChangePlayer()
+      //4. MakeMove()
+      //5. SpaceInUse()
+        }
+        public static string player = "x";
+       public static string[][] array1;
+        public static string[][] Creategrid()
+        {
+            string[][] Board1 = new string[3][];
+            for (int i = 0; i < Board1.GetLength(0); i++)
+            {
+                Board1[i] = new string[3];
+            }
+            return Board1;
+        }
+
+
+        public static string CurrentPlayer()
+        {
+            return player;
+        }
+        public static void ChangePlayer()
+        {
+            if (player == "x")
+            {
+                player = "y";
+            }
+            else player = "x";
+        }
+        public static void MakeMove(int x, int y)
+        {
+            array1[x][y] = CurrentPlayer();
+            ChangePlayer();
+        }
+        public static Boolean SpaceInUse(int x, int y)
+        {
+            try
+            {
+                if (array1[x][y] == "x" || array1[x][y] == "y")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Dull_Lab_8/Dull_Lab_8Tests/ProgramTests.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Dull_Lab_8;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dull_Lab_8.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void CreategridTest1()
+        {
+            
+        Program.array1 = Program.Creategrid();//set to default
+            Boolean test = false;
+            try
+            {
+                if (Program.array1[0][0] == "" || Program.array1[0][1] == "" || Program.array1[0][2] == "" || Program.array1[1][0] == "" || Program.array1[1][1] == "" || Program.array1[1][2] == "" || Program.array1[2][0] == "" || Program.array1[2][1] == "" || Program.array1[2][2] == "")
+                {
+                    test = true;
+                }
+                test = true;
+            }
+            catch
+            {
+                test = false;
+            }
+                Assert.IsTrue(test);
+        }
+
+        [TestMethod()]
+        public void CurrentPlayerTest1()
+        {
+            Program.player = "x"; //set to default
+           
+            Assert.AreEqual(Program.CurrentPlayer(),"x");
+        }
+
+        [TestMethod()]
+        public void ChangePlayerTest1()
+        {
+            Program.player = "x";//set to default
+            Program.ChangePlayer();
+                Assert.AreEqual(Program.CurrentPlayer(), "y");
+        }
+
+        [TestMethod()]
+        public void MakeMoveTestx()
+        {
+            Program.player = "x";//set to default
+
+            Program.array1 = Program.Creategrid();
+            Program.MakeMove(0, 0);
+            Assert.AreEqual(Program.array1[0][0], "x");
+        }
+        [TestMethod()]
+        public void MakeMoveTesty()
+        {
+            Program.player = "x";//set to default
+            Program.ChangePlayer();
+            Program.array1 = Program.Creategrid();
+            Program.MakeMove(0, 0);
+            Assert.AreEqual(Program.array1[0][0], "y");
+
+        }
+
+        [TestMethod()]
+        public void SpaceInUseTest()
+        {
+            Program.array1 = Program.Creategrid();//set to default
+
+
+            Assert.IsTrue(!Program.SpaceInUse(0,0));
+        }
+    }
+}
